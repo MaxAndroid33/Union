7a9d57df5c251ccb2a775dde02cb7b9c7ff84f7a
MaxAndroid33 <yuosifmax309@gmail.com>
2022-09-29 22:30:35 +0300

    add some feutuere to nav

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
 
 const navbarNav =document.querySelector('.navbar-nav');
 const navbarToggleBtn =document.querySelector('.nav-toggle-btn');
+const animetion =document.querySelector('')
 
 
 //navBarToggle functions 
